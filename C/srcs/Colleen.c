d7c66e5af32fd527ab50f49fdf905d2003e522c7
Antoine GOUBY <agouby@e2r2p16.42.fr>
2018-02-10 02:46:14 +0100

    starting sully in c

--- C/srcs/Colleen.c
+++ C/srcs/Colleen.c
@@ -1,2 +1,2 @@
 #include <stdio.h>
-void	foo(void){return ;}/*Hello*/int main(void){/*Do nothing*/char *str = "#include <stdio.h>%1$cvoid%2$cfoo(void){return ;}/*Hello*/int main(void){/*Do nothing*/char *str = %3$c%4$s%3$c;foo();printf(str, 10, 9, 34, str);return (0);}%1$c";foo();printf(str, 10, 9, 34, str);return (0);}
+void	foo(void){return;}/*Hello*/int main(void){/*Do nothing*/char *str="#include <stdio.h>%1$cvoid%2$cfoo(void){return;}/*Hello*/int main(void){/*Do nothing*/char *str=%3$c%4$s%3$c;foo();printf(str,10,9,34,str);return(0);}%1$c";foo();printf(str,10,9,34,str);return(0);}
--- C/srcs/Sully.c
+++ C/srcs/Sully.c
@@ -1,11 +1,16 @@
 #include <stdio.h>
 #include <fcntl.h>
-
+#include <stdlib.h>
+#include <string.h>
+#define OPEN(X) char buf[1024];sprintf(buf, "Sully_%d.c", X);int fd=open(buf,O_RDWR|O_CREAT,0644);if(fd==-1)return(-1)
+#define COMPILE char buf2[1024];sprintf(buf2, "gcc -Wall -Werror -Wextra -o %2$.*1$s %2$s", (int)strlen(buf)-2,buf);system(buf2)
 int main(void)
 {
-	char	buf[4096];
-
-	sprintf(buf, "Sully_%d.c", 1);
-	int fd = open(buf, O_RDWR|O_CREAT, 0644);
+	int i=5;
+	OPEN(i);
+	i--;
+	if(i==0)
+		return(0);
+	COMPILE;
 	return (0);
 }
