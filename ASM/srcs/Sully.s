faafe191bcc3dadd4384ff2910efd08bd0463471
Antoine GOUBY <agouby@e3r1p6.42.fr>
2019-03-06 18:52:49 +0100

    Done

--- ASM/srcs/Sully.s
+++ ASM/srcs/Sully.s
@@ -5,13 +5,7 @@ extern _dprintf
 extern _system
 extern _strlen
 
-%macro OPEN 1
-lea rdi,[rel buf]
-lea rsi,[rel open_str]
-mov rdx,%1
-sub rsp,0x8
-call _sprintf
-add rsp,0x8
+%macro OPEN 0
 mov rax,0x2000005
 lea rdi,[rel buf]
 mov rsi,514
@@ -37,9 +31,7 @@ mov rdx,0xA
 lea rcx,[rel str]
 mov r8,0x22
 pop r9
-sub rsp,0x8
 push r9
-dec r9
 call _dprintf
 
 lea rdi,[rel buf2]
@@ -92,17 +84,37 @@ call _system
 %endmacro
 
 _main:
-sub rsp,0x8
-mov rax,5
+mov rax,100
 push rax
 and rax,rax
 jle _exit
-OPEN rax
+lea rdi,[rel buf]
+lea rsi,[rel open_str]
+mov rdx,rax
+call _sprintf
+mov rax,0x2000021
+lea rdi,[rel buf]
+mov rsi,0
+syscall
+cmp rax,0
+je _exists
+_decif:
+OPEN
 COMPILE
 _exit:
 mov rax,0x2000001
 xor rdi,rdi
 syscall
+
+_exists:
+pop r9
+dec r9
+push r9
+lea rdi,[rel buf]
+lea rsi,[rel open_str]
+mov rdx,r9
+call _sprintf
+jmp _decif
 
 section .data
 buf: times 1024 db 0x0
@@ -112,4 +124,4 @@ del: db "rm -f Sully_%d.o",0x0
 cmp: db "nasm -f macho64 %s",0x0
 launch: db "./Sully_%d",0x0
 lnk: db "ld -o %2$.*1$s %2$.*1$s.o -arch x86_64 -macosx_version_min 16.7.0 -lc",0x0
-str: db "section .text%1$cglobal _main%1$cextern _sprintf%1$cextern _dprintf%1$cextern _system%1$cextern _strlen%1$c%1$c%%macro OPEN 1%1$clea rdi,[rel buf]%1$clea rsi,[rel open_str]%1$cmov rdx,%%1%1$csub rsp,0x8%1$ccall _sprintf%1$cadd rsp,0x8%1$cmov rax,0x2000005%1$clea rdi,[rel buf]%1$cmov rsi,514%1$cmov rdx,0q644%1$csyscall%1$cpush rax%1$c%%endmacro%1$c%1$c_init:%1$cinc rcx%1$ccmp rcx, 0x400%1$cje _ret%1$cmov byte[rdi + rcx],0x0%1$cjmp _init%1$c%1$c_ret:%1$cret%1$c%1$c%%macro COMPILE 0%1$cpop rdi%1$clea rsi,[rel str]%1$cmov rdx,0xA%1$clea rcx,[rel str]%1$cmov r8,0x22%1$cpop r9%1$csub rsp,0x8%1$cpush r9%1$cdec r9%1$ccall _dprintf%1$c%1$clea rdi,[rel buf2]%1$clea rsi,[rel cmp]%1$clea rdx,[rel buf]%1$ccall _sprintf%1$c%1$clea rdi,[rel buf]%1$ccall _strlen%1$c%1$csub rax,0x2%1$csub rsp,0x8%1$cpush rax%1$clea rdi,[rel buf2]%1$ccall _system%1$c%1$clea rdi,[rel buf2]%1$cmov rcx,0x0%1$ccall _init%1$cxor rax,rax%1$cpop rdx%1$cadd rsp,0x8%1$clea rsi,[rel lnk]%1$clea rcx,[rel buf]%1$ccall _sprintf%1$c%1$clea rdi,[rel buf2]%1$ccall _system%1$c%1$cxor rcx,rcx%1$clea rdi,[rel buf2]%1$ccall _init%1$clea rsi,[rel del]%1$cpop rdx%1$cpush rdx%1$ccall _sprintf%1$clea rdi,[rel buf2]%1$ccall _system%1$c%1$cxor rcx,rcx%1$clea rdi,[rel buf2]%1$ccall _init%1$cpop rdx%1$clea rsi,[rel launch]%1$ccall _sprintf%1$cadd rsp,0x8%1$clea rdi,[rel buf2]%1$ccall _system%1$c%1$c%%endmacro%1$c%1$c_main:%1$csub rsp,0x8%1$cmov rax,%4$d%1$cpush rax%1$cand rax,rax%1$cjle _exit%1$cOPEN rax%1$cCOMPILE%1$c_exit:%1$cmov rax,0x2000001%1$cxor rdi,rdi%1$csyscall%1$c%1$csection .data%1$cbuf: times 1024 db 0x0%1$cbuf2: times 1024 db 0x0%1$copen_str: db %3$cSully_%%d.s%3$c,0x0%1$cdel: db %3$crm -f Sully_%%d.o%3$c,0x0%1$ccmp: db %3$cnasm -f macho64 %%s%3$c,0x0%1$claunch: db %3$c./Sully_%%d%3$c,0x0%1$clnk: db %3$cld -o %%2$.*1$s %%2$.*1$s.o -arch x86_64 -macosx_version_min 16.7.0 -lc%3$c,0x0%1$cstr: db %3$c%2$s%3$c%1$c"
+str: db "section .text%1$cglobal _main%1$cextern _sprintf%1$cextern _dprintf%1$cextern _system%1$cextern _strlen%1$c%1$c%%macro OPEN 0%1$cmov rax,0x2000005%1$clea rdi,[rel buf]%1$cmov rsi,514%1$cmov rdx,0q644%1$csyscall%1$cpush rax%1$c%%endmacro%1$c%1$c_init:%1$cinc rcx%1$ccmp rcx, 0x400%1$cje _ret%1$cmov byte[rdi + rcx],0x0%1$cjmp _init%1$c%1$c_ret:%1$cret%1$c%1$c%%macro COMPILE 0%1$cpop rdi%1$clea rsi,[rel str]%1$cmov rdx,0xA%1$clea rcx,[rel str]%1$cmov r8,0x22%1$cpop r9%1$cpush r9%1$ccall _dprintf%1$c%1$clea rdi,[rel buf2]%1$clea rsi,[rel cmp]%1$clea rdx,[rel buf]%1$ccall _sprintf%1$c%1$clea rdi,[rel buf]%1$ccall _strlen%1$c%1$csub rax,0x2%1$csub rsp,0x8%1$cpush rax%1$clea rdi,[rel buf2]%1$ccall _system%1$c%1$clea rdi,[rel buf2]%1$cmov rcx,0x0%1$ccall _init%1$cxor rax,rax%1$cpop rdx%1$cadd rsp,0x8%1$clea rsi,[rel lnk]%1$clea rcx,[rel buf]%1$ccall _sprintf%1$c%1$clea rdi,[rel buf2]%1$ccall _system%1$c%1$cxor rcx,rcx%1$clea rdi,[rel buf2]%1$ccall _init%1$clea rsi,[rel del]%1$cpop rdx%1$cpush rdx%1$ccall _sprintf%1$clea rdi,[rel buf2]%1$ccall _system%1$c%1$cxor rcx,rcx%1$clea rdi,[rel buf2]%1$ccall _init%1$cpop rdx%1$clea rsi,[rel launch]%1$ccall _sprintf%1$cadd rsp,0x8%1$clea rdi,[rel buf2]%1$ccall _system%1$c%1$c%%endmacro%1$c%1$c_main:%1$cmov rax,%4$d%1$cpush rax%1$cand rax,rax%1$cjle _exit%1$clea rdi,[rel buf]%1$clea rsi,[rel open_str]%1$cmov rdx,rax%1$ccall _sprintf%1$cmov rax,0x2000021%1$clea rdi,[rel buf]%1$cmov rsi,0%1$csyscall%1$ccmp rax,0%1$cje _exists%1$c_decif:%1$cOPEN%1$cCOMPILE%1$c_exit:%1$cmov rax,0x2000001%1$cxor rdi,rdi%1$csyscall%1$c%1$c_exists:%1$cpop r9%1$cdec r9%1$cpush r9%1$clea rdi,[rel buf]%1$clea rsi,[rel open_str]%1$cmov rdx,r9%1$ccall _sprintf%1$cjmp _decif%1$c%1$csection .data%1$cbuf: times 1024 db 0x0%1$cbuf2: times 1024 db 0x0%1$copen_str: db %3$cSully_%%d.s%3$c,0x0%1$cdel: db %3$crm -f Sully_%%d.o%3$c,0x0%1$ccmp: db %3$cnasm -f macho64 %%s%3$c,0x0%1$claunch: db %3$c./Sully_%%d%3$c,0x0%1$clnk: db %3$cld -o %%2$.*1$s %%2$.*1$s.o -arch x86_64 -macosx_version_min 16.7.0 -lc%3$c,0x0%1$cstr: db %3$c%2$s%3$c%1$c"
--- C/srcs/Sully.c
+++ C/srcs/Sully.c
@@ -2,9 +2,10 @@
 #include<fcntl.h>
 #include<stdlib.h>
 #include<string.h>
-#define OPEN char buf[1024];sprintf(buf,"Sully_%d.c",i);int fd=open(buf,O_RDWR|O_CREAT,0644);if(fd==-1)return(-1)
+#include<unistd.h>
+#define OPEN int fd=open(buf,O_RDWR|O_CREAT,0644);if(fd==-1)return(-1)
 #define COMPILE char buf2[1024];sprintf(buf2,"gcc -Wall -Werror -Wextra -o %2$.*1$s %2$s",(int)strlen(buf)-2,buf);system(buf2)
-#define COPY char *str="#include<stdio.h>%1$c#include<fcntl.h>%1$c#include<stdlib.h>%1$c#include<string.h>%1$c#define OPEN char buf[1024];sprintf(buf,%2$cSully_%%d.c%2$c,i);int fd=open(buf,O_RDWR|O_CREAT,0644);if(fd==-1)return(-1)%1$c#define COMPILE char buf2[1024];sprintf(buf2,%2$cgcc -Wall -Werror -Wextra -o %%2$.*1$s %%2$s%2$c,(int)strlen(buf)-2,buf);system(buf2)%1$c#define COPY char *str=%2$c%3$s%2$c%1$c#define EXECUTE sprintf(buf2,%2$c./%%.*s%2$c,(int)strlen(buf)-2,buf);system(buf2)%1$c#define MAIN(X) int main(void){int i=X;if(X<=0)return(0);OPEN;--i;COPY;dprintf(fd,str,10,34,str,i);COMPILE;EXECUTE;return(0);}%1$cMAIN(%4$d)%1$c"
+#define COPY char *str="#include<stdio.h>%1$c#include<fcntl.h>%1$c#include<stdlib.h>%1$c#include<string.h>%1$c#include<unistd.h>%1$c#define OPEN int fd=open(buf,O_RDWR|O_CREAT,0644);if(fd==-1)return(-1)%1$c#define COMPILE char buf2[1024];sprintf(buf2,%2$cgcc -Wall -Werror -Wextra -o %%2$.*1$s %%2$s%2$c,(int)strlen(buf)-2,buf);system(buf2)%1$c#define COPY char *str=%2$c%3$s%2$c%1$c#define EXECUTE sprintf(buf2,%2$c./%%.*s%2$c,(int)strlen(buf)-2,buf);system(buf2)%1$c#define MAIN(X) int main(void){int i=X;if(X<=0)return(0);char buf[1024];sprintf(buf,%2$cSully_%%d.c%2$c, i);if(access(buf, F_OK)==0){i--;sprintf(buf,%2$cSully_%%d.c%2$c,i);}OPEN;COPY;dprintf(fd,str,10,34,str,i);COMPILE;EXECUTE;return(0);}%1$cMAIN(%4$d)%1$c"
 #define EXECUTE sprintf(buf2,"./%.*s",(int)strlen(buf)-2,buf);system(buf2)
-#define MAIN(X) int main(void){int i=X;if(X<=0)return(0);OPEN;--i;COPY;dprintf(fd,str,10,34,str,i);COMPILE;EXECUTE;return(0);}
-MAIN(10)
+#define MAIN(X) int main(void){int i=X;if(X<=0)return(0);char buf[1024];sprintf(buf,"Sully_%d.c", i);if(access(buf, F_OK)==0){i--;sprintf(buf,"Sully_%d.c",i);}OPEN;COPY;dprintf(fd,str,10,34,str,i);COMPILE;EXECUTE;return(0);}
+MAIN(5)
